c176503675b14c4848246febfc83f62e4262aff5
alh0059 <alh0059@mix.wvu.edu>
2022-05-07 19:00:06 -0400

    comments & readMe

---
+++ README.md
@@ -0,0 +1,1 @@
+<h3 style="text-align:center">Python Video Muter using ffmpeg</h3>
--- mute.py
+++ mute.py
@@ -1,8 +1,8 @@
 import os
 import time
  
-original_clip_directory = "D:\SAR Video Backup/Nates_SD"
-muted_clip_directory  = "D:\SAR Video Backup/Nates_SD/Nates_SD_Muted"
+original_clip_directory = "D:\Video Backup"
+muted_clip_directory    = "D:\Video Backup\Muted"
 
 ignored_extensions = [".sfk", ".jpg", ".cr2"]
 
@@ -10,6 +10,7 @@ print(f"Looping all files and directories in the specified path: {original_clip_
 
 for filename in os.listdir(original_clip_directory):
     
+    # Check file extension and skip if it isn't a video format. 
     file_extension = filename[len(filename)-4 : len(filename)]
 
     if file_extension.lower() in ignored_extensions:
@@ -17,16 +18,12 @@ for filename in os.listdir(original_clip_directory):
 
     else:
 
-        full_path = os.path.join(original_clip_directory,filename)
+        current_file_path = os.path.join(original_clip_directory,filename)
     
-        if os.path.isfile(full_path):
+        if os.path.isfile(current_file_path):
         
-            current_file_path = '%s\%s' % (original_clip_directory, filename)
             muted_file_path = "%s\%s" % (muted_clip_directory, filename)
 
-            print(current_file_path)
-            print(muted_file_path)
-            
             os.system('ffmpeg -i "%s" -c copy -an "%s"' % (current_file_path, muted_file_path))
 
             time.sleep(0.25)
